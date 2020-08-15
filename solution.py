5fdb96c8f77e1bbd636eaff23ccc110b09387cda
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-15 21:25:55 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,8 @@
+from collections import defaultdict
 
+d = defaultdict(list)
+d['python'].append("awesome")
+d['something-else'].append("not relevant")
+d['python'].append("language")
+for i in d.items():
+    print i
