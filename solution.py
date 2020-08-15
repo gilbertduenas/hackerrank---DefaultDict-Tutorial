1044a95d979fccec91c1d349abb4180a37a9b6d1
Gilbert <gilbertduenas@users.noreply.github.com>
2020-08-15 22:02:39 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,3 +6,22 @@ d['something-else'].append("not relevant")
 d['python'].append("language")
 for i in d.items():
     print i
+
+from collections import defaultdict
+
+n, m = map(int, input().split())
+
+a = defaultdict(list)
+b = []
+
+for i in range(n):
+    a[input()].append(i+1)
+
+for i in range(m):
+    b.append(input())
+
+for i in b: 
+    if i in a:
+        print(' '.join(map(str, a[i])))
+    else:
+        print(-1)
